765c4bb1c78117983b2e14e8700581b2847ed405
Jelmer Hinssen <jelmerhinssen@hotmail.com>
2023-09-28 20:50:39 +0200

    Non nested yielding

--- src/include/promise.h
+++ src/include/promise.h
@@ -1,143 +1,167 @@
 #pragma once
-#include <coroutine>
 #include <concepts>
-#include <type_traits>
+#include <coroutine>
 #include <optional>
+#include <type_traits>
 #include <unordered_set>
+#include <cassert>
 
 namespace promise {
 
-template <typename T> class SuspensionPoint;
-template <typename R, typename Y> class Promise;
+template <typename T>
+class SuspensionPoint;
+template <typename R, typename Y>
+class Promise;
 
 namespace detail {
-    template <typename T>
-    struct promise_helper {
-        using type = std::optional<T>;
-    };
-    template <>
-    struct promise_helper<void> {
-        using type = bool;
-    };
-}
+template <typename T>
+struct promise_helper {
+    using type = std::optional<T>;
+};
+template <>
+struct promise_helper<void> {
+    using type = bool;
+};
+}  // namespace detail
 
 template <typename T>
 using optional = detail::promise_helper<T>::type;
 
 class Coroutine {
-    public:
-    std::suspend_always initial_suspend() const noexcept {return {};}
-    std::suspend_always final_suspend() const noexcept {return {};}
+   public:
+    std::suspend_always initial_suspend() const noexcept { return {}; }
+    std::suspend_always final_suspend() const noexcept { return {}; }
     void unhandled_exception() {}
-    #ifdef TEST
+#ifdef TEST
     inline static std::unordered_set<const Coroutine*> living = {};
-    Coroutine() {living.insert(this);}
-    ~Coroutine() {living.erase(this);}
-    #endif
+    Coroutine() {
+        auto it = living.find(this);
+        EXPECT_EQ(it, living.end());
+        living.insert(this);
+    }
+    ~Coroutine() {
+        auto it = living.find(this);
+        EXPECT_NE(it, living.end());
+        living.erase(this);
+    }
+#endif
     friend class CoroutineHandle;
-    protected:
+
+   protected:
     bool m_yielded = false;
-    private:
+
+   private:
     using Handle = std::coroutine_handle<Coroutine>;
-    void gain_ref() {ref_count++;}
-    void lose_ref() {if (!--ref_count) handle().destroy();}
-    inline Handle handle() noexcept {return Handle::from_promise(*this);}
+    void gain_ref() { ref_count++; }
+    void lose_ref() {
+        if (!--ref_count) handle().destroy();
+    }
+    inline Handle handle() noexcept { return Handle::from_promise(*this); }
     int ref_count = 0;
 };
 template <typename Y>
 class YieldingCoroutine : public Coroutine {
-public:
-    template <typename T> std::suspend_always yield_value(T&& arg) {
+   public:
+    template <typename T>
+    std::suspend_always yield_value(T&& arg) {
         m_yield_value = std::forward<T>(arg);
         m_yielded = true;
         return {};
     }
-private:
+    template <typename R>
+    auto await_transform(Promise<R, Y>&& r) {
+        return r;
+    }
+   private:
     optional<Y> m_yield_value;
-    template <typename R, typename Y1> friend class Promise;
+    template <typename R, typename Y1>
+    friend class Promise;
 };
 
 namespace detail {
 
 template <typename R>
 class ReturnValue {
-public: 
-    template <typename T> void return_value(T&& arg) {m_return_value = std::forward<T>(arg);}
-protected:
+   public:
+    template <typename T>
+    void return_value(T&& arg) {
+        m_return_value = std::forward<T>(arg);
+    }
+
+   protected:
     optional<R> m_return_value;
-    template <typename R1, typename Y> friend class Promise;
+    template <typename R1, typename Y>
+    friend class Promise;
 };
 
 template <>
 class ReturnValue<void> {
-public: 
-    void return_void() {m_return_value = true;}
-protected:
+   public:
+    void return_void() { m_return_value = true; }
+
+   protected:
     optional<void> m_return_value;
 
-    template <typename R, typename Y> friend class Promise;
+    template <typename R, typename Y>
+    friend class Promise;
 };
 
-}
+}  // namespace detail
 
 template <typename R, typename Y>
 class ReturningCoroutine : public YieldingCoroutine<Y>, public detail::ReturnValue<R> {
-public:
+   public:
     Promise<R, Y> get_return_object();
-private:
-    
-    
+
+   private:
 };
 
 class CoroutineHandle {
-public:
-    CoroutineHandle(Coroutine& handle): 
-        m_coroutine(handle),
-        m_started(false) {
-        m_coroutine.gain_ref();
+   public:
+    CoroutineHandle(Coroutine& handle) : m_coroutine(handle), m_started(false) { m_coroutine.gain_ref(); }
+    ~CoroutineHandle() { m_coroutine.lose_ref(); }
+    bool done() const noexcept { return m_coroutine.handle().done(); }
+    bool started() const noexcept { return m_started; }
+    bool yielded() const noexcept { return m_coroutine.m_yielded; }
+    void start() {
+        m_started = true;
+        m_coroutine.handle().resume();
     }
-    ~CoroutineHandle() { 
-        m_coroutine.lose_ref();
+    void resume() {
+        m_coroutine.m_yielded = false;
+        m_coroutine.handle().resume();
     }
-    bool done() const noexcept {return m_coroutine.handle().done();}
-    bool started() const noexcept {return m_started;}
-    bool yielded() const noexcept {return m_coroutine.m_yielded;}
-    void start() {m_started = true; m_coroutine.handle().resume();}
-    void resume() {m_coroutine.m_yielded = false; m_coroutine.handle().resume();}
-private:
+
+   protected:
     Coroutine& m_coroutine;
     bool m_started;
-
 };
 
 template <typename R, typename Y = void>
 class Promise : public CoroutineHandle {
-public:
-    Promise(ReturningCoroutine<R, Y>& handle): 
-        CoroutineHandle(handle), 
-        m_return_value(handle.m_return_value),
-        m_yield_value(handle.m_yield_value) {}
-    // auto then(auto f) {
-    //     if constexpr (std::is_void_v<R>) {
-    //         co_await *this;
-    //         co_return f();
-    //     } else {
-    //         co_return f(co_await *this);
-    //     }
-    // }
-    // template <typename R2, Y>
-    // Promise<R2, Y> then(auto f) {
-    //     if constexpr (std::is_void_v<R>) {
-    //         co_await *this;
-    //         co_return co_await f();
-    //     } else {
-    //         co_return co_await f(co_await *this);
-    //     }
-    // }
-    optional<Y> yield_value() const noexcept {return m_yield_value;}
-    optional<R> return_value() const noexcept {return m_return_value;}
+   public:
+    Promise(ReturningCoroutine<R, Y>& handle)
+        : CoroutineHandle(handle), m_return_value(handle.m_return_value), m_yield_value(handle.m_yield_value) {}
+    optional<Y> yield_value() const noexcept { return m_yield_value; }
+    optional<R> return_value() const noexcept { return m_return_value; }
     using promise_type = ReturningCoroutine<R, Y>;
-private:
+    bool await_ready() {
+        start();
+        bool should_supsend = !done();
+        return !should_supsend;
+    }
+    bool await_suspend(auto handle) {
+        bool should_suspend = true;
+        return should_suspend;
+    }
+    R await_resume() {
+        if constexpr (!std::is_void_v<R>) {
+            if (!m_return_value) throw std::runtime_error("Function did not return a value");
+            return *m_return_value;
+        }
+    }
+
+   private:
     optional<R>& m_return_value;
     optional<Y>& m_yield_value;
 };
@@ -147,4 +171,4 @@ Promise<R, Y> ReturningCoroutine<R, Y>::get_return_object() {
     return {*this};
 }
 
-}
+}  // namespace promise
--- test/promise.cpp
+++ test/promise.cpp
@@ -1,12 +1,31 @@
 #include <gtest/gtest.h>
+#include <array>
 #include "promise.h"
 
 using namespace promise;
+using namespace std;
 
 static auto& living = promise::Coroutine::living;
 
 class PromiseTest : public testing::Test {
 public:
+    enum FunctionNames {
+        EMPTY_CO,
+        EMPTY_RETURNING,
+        CONDITIONAL_RETURNING,
+        YIELDING_CO_0,
+        YIELDING_CO_1,
+        NESTED_EMPTY_0,
+        NESTED_EMPTY_1,
+        NESTED_YIELDING_0,
+        NESTED_YIELDING_1,
+        NESTED_YIELDING_2,
+        FUNCTION_COUNT
+    };
+
+    array<int, FUNCTION_COUNT> function_counts = {};
+    array<int, FUNCTION_COUNT> expected_counts = {};
+
     PromiseTest() {
         living.clear();
     }
@@ -14,22 +33,42 @@ public:
         EXPECT_TRUE(living.empty());
     }
     Promise<void> empty_co (){
+        function_counts[EMPTY_CO]++;
         co_return;
     }
     Promise<int> empty_returning (){
+        function_counts[EMPTY_RETURNING]++;
         co_return 1;
     }
     Promise<int> conditional_returning (bool b) {
+        function_counts[CONDITIONAL_RETURNING]++;
         if (b) co_return 1;
     }
     Promise<void, int> yielding_co() {
+        function_counts[YIELDING_CO_0]++;
         co_yield 5;
+        function_counts[YIELDING_CO_1]++;
+        co_return;
+    }
+    Promise<void> nested_empty() {
+        function_counts[NESTED_EMPTY_0]++;
+        co_await empty_co();
+        function_counts[NESTED_EMPTY_1]++;
+        co_return;
+    }
+    Promise<void, int> nested_yielding() {
+        function_counts[NESTED_YIELDING_0]++;
+        co_await yielding_co();
+        function_counts[NESTED_YIELDING_1]++;
+        co_await yielding_co();
+        function_counts[NESTED_YIELDING_2]++;
         co_return;
     }
 };
 
 TEST_F(PromiseTest, emptyCoroutine) {
     auto p = empty_co();
+    EXPECT_EQ(function_counts, expected_counts);
     EXPECT_EQ(living.size(), 1);
     EXPECT_FALSE(p.started());
     EXPECT_FALSE(p.done());
@@ -40,6 +79,7 @@ TEST_F(PromiseTest, emptyCoroutine) {
 
 TEST_F(PromiseTest, emptyReturningCoroutine) {
     auto p = empty_returning();
+    EXPECT_EQ(function_counts, expected_counts);
     EXPECT_EQ(living.size(), 1);
     EXPECT_FALSE(p.started());
     EXPECT_FALSE(p.done());
@@ -51,6 +91,8 @@ TEST_F(PromiseTest, emptyReturningCoroutine) {
 TEST_F(PromiseTest, startEmptyReturningCoroutine) {
     auto p = empty_returning();
     p.start();
+    expected_counts[EMPTY_RETURNING]++;
+    EXPECT_EQ(function_counts, expected_counts);
     EXPECT_EQ(living.size(), 1);
     EXPECT_TRUE(p.started());
     EXPECT_TRUE(p.done());
@@ -62,6 +104,8 @@ TEST_F(PromiseTest, startEmptyReturningCoroutine) {
 TEST_F(PromiseTest, startYieldingCoroutine) {
     auto p = yielding_co();
     p.start();
+    expected_counts[YIELDING_CO_0]++;
+    EXPECT_EQ(function_counts, expected_counts);
     EXPECT_EQ(living.size(), 1);
     EXPECT_TRUE(p.started());
     EXPECT_FALSE(p.done());
@@ -73,6 +117,8 @@ TEST_F(PromiseTest, startYieldingCoroutine) {
 TEST_F(PromiseTest, resumeYieldingCoroutine) {
     auto p = yielding_co();
     p.start();
+    expected_counts[YIELDING_CO_0]++;
+    EXPECT_EQ(function_counts, expected_counts);
     EXPECT_EQ(living.size(), 1);
     EXPECT_TRUE(p.started());
     EXPECT_FALSE(p.done());
@@ -80,6 +126,8 @@ TEST_F(PromiseTest, resumeYieldingCoroutine) {
     EXPECT_EQ(p.yield_value(), 5);
     EXPECT_TRUE(p.yielded());
     p.resume();
+    expected_counts[YIELDING_CO_1]++;
+    EXPECT_EQ(function_counts, expected_counts);
     EXPECT_EQ(living.size(), 1);
     EXPECT_TRUE(p.started());
     EXPECT_TRUE(p.done());
@@ -91,6 +139,8 @@ TEST_F(PromiseTest, resumeYieldingCoroutine) {
 TEST_F(PromiseTest, startConditionalReturningCoroutine) {
     auto p = conditional_returning(false);
     p.start();
+    expected_counts[CONDITIONAL_RETURNING]++;
+    EXPECT_EQ(function_counts, expected_counts);
     EXPECT_EQ(living.size(), 1);
     EXPECT_TRUE(p.started());
     EXPECT_TRUE(p.done());
@@ -102,10 +152,64 @@ TEST_F(PromiseTest, startConditionalReturningCoroutine) {
 TEST_F(PromiseTest, startEmptyCoroutine) {
     auto p = empty_co();
     p.start();
+    expected_counts[EMPTY_CO]++;
+    EXPECT_EQ(function_counts, expected_counts);
     EXPECT_EQ(living.size(), 1);
     EXPECT_TRUE(p.started());
     EXPECT_TRUE(p.done());
     EXPECT_TRUE(p.return_value());
     EXPECT_FALSE(p.yield_value());
+    EXPECT_FALSE(p.yielded());
+}
+
+TEST_F(PromiseTest, nestedCoroutine) {
+    auto p = nested_empty();
+    p.start();
+    expected_counts[NESTED_EMPTY_0]++;
+    expected_counts[EMPTY_CO]++;
+    expected_counts[NESTED_EMPTY_1]++;
+    EXPECT_EQ(function_counts, expected_counts);
+    EXPECT_EQ(living.size(), 1);
+    EXPECT_TRUE(p.started());
+    EXPECT_TRUE(p.done());
+    EXPECT_TRUE(p.return_value());
+    EXPECT_FALSE(p.yield_value());
+    EXPECT_FALSE(p.yielded());
+}
+
+TEST_F(PromiseTest, nestedYieldingCoroutine) {
+    auto p = nested_yielding();
+    p.start();
+    expected_counts[NESTED_YIELDING_0]++;
+    expected_counts[YIELDING_CO_0]++;
+    EXPECT_EQ(function_counts, expected_counts);
+    EXPECT_EQ(living.size(), 2);
+    EXPECT_TRUE(p.started());
+    EXPECT_FALSE(p.done());
+    EXPECT_FALSE(p.return_value());
+    EXPECT_EQ(p.yield_value(), 5);
+    EXPECT_TRUE(p.yielded());
+
+    p.resume();
+    expected_counts[YIELDING_CO_1]++;
+    expected_counts[NESTED_YIELDING_1]++;
+    expected_counts[YIELDING_CO_0]++;
+    EXPECT_EQ(function_counts, expected_counts);
+    EXPECT_EQ(living.size(), 2);
+    EXPECT_TRUE(p.started());
+    EXPECT_FALSE(p.done());
+    EXPECT_FALSE(p.return_value());
+    EXPECT_EQ(p.yield_value(), 5);
+    EXPECT_TRUE(p.yielded());
+
+    p.resume();
+    expected_counts[YIELDING_CO_1]++;
+    expected_counts[NESTED_YIELDING_2]++;
+    EXPECT_EQ(function_counts, expected_counts);
+    EXPECT_EQ(living.size(), 1);
+    EXPECT_TRUE(p.started());
+    EXPECT_TRUE(p.done());
+    EXPECT_TRUE(p.return_value());
+    EXPECT_EQ(p.yield_value(), 5); // ?
     EXPECT_FALSE(p.yielded());
 }
