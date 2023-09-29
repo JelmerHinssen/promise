d6aee8d55d30b99a751612a8e035337e45f825e0
Jelmer Hinssen <jelmerhinssen@hotmail.com>
2023-09-29 09:43:25 +0200

    Inplace optional

--- src/include/optional.h
+++ src/include/optional.h
@@ -1,11 +1,71 @@
 #pragma once
 #include <optional>
+#include <type_traits>
 
 namespace promise {
 namespace detail {
+
 template <typename T>
-struct optional_helper {
+concept is_assignable = std::is_move_assignable_v<T> || std::is_copy_assignable_v<T>;
+template <typename T>
+concept is_not_assignable = !is_assignable<T>;
+
+template <typename T>
+struct optional_helper;
+
+template <is_assignable T>
+struct optional_helper<T> {
     using type = std::optional<T>;
+};
+
+template <typename T>
+class inplace_optional {
+   public:
+    struct Dummy {
+        constexpr Dummy() noexcept {}
+    };
+    union {
+        Dummy m_dummy;
+        T m_value;
+    };
+    bool m_has_value;
+    operator bool() const noexcept { return m_has_value; }
+    inplace_optional() : m_dummy{}, m_has_value(false) {}
+    template <typename... Args>
+    inplace_optional(Args&&... args)
+        requires(sizeof...(Args) > 0)
+        : m_value(std::forward<Args>(args)...), m_has_value(true) {}
+    ~inplace_optional() { destroy(); }
+    T& operator*() & { return m_value; }
+    T* operator->() { return &m_value; }
+    const T& operator*() const& { return m_value; }
+    const T* operator->() const { return &m_value; }
+    T&& operator*() && { return std::move(m_value); }
+    template <typename... Args>
+    void assign(Args&&... args) {
+        reset();
+        new (&m_value) T(std::forward<Args>(args)...);
+        m_has_value = true;
+    }
+    template <typename Arg>
+    inplace_optional& operator=(Arg&& arg) {
+        assign(std::forward<Arg>(arg));
+        return *this;
+    }
+    void reset() {
+        destroy();
+        m_has_value = false;
+    }
+
+   private:
+    void destroy() {
+        if (m_has_value) m_value.~T();
+    }
+};
+
+template <is_not_assignable T>
+struct optional_helper<T> {
+    using type = inplace_optional<T>;
 };
 template <>
 struct optional_helper<void> {
--- test/optional.cpp
+++ test/optional.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
+
 #include <array>
+
 #include "promise.h"
 
 using namespace promise;
@@ -27,17 +29,87 @@ TEST(Optional, nonEmptyInt) {
 
 struct Unassignable {
     int& x;
+    inline static int dtor_count = 0;
+    inline static int living_count = 0;
+    Unassignable(int& x) : x(x) { living_count++; }
+    Unassignable(const Unassignable& other) : x(other.x) { living_count++; }
+    ~Unassignable() {
+        living_count--;
+        dtor_count++;
+    }
+};
+class UnassignableTest : public testing::Test {
+   public:
+    int& dtor_count = Unassignable::dtor_count;
+    int& living_count = Unassignable::living_count;
+    UnassignableTest() {
+        dtor_count = 0;
+        living_count = 0;
+    }
+    ~UnassignableTest() { EXPECT_EQ(living_count, 0); }
 };
 
-TEST(Optional, emptyUnassignable) {
+TEST_F(UnassignableTest, emptyUnassignable) {
     optional<Unassignable> x{};
     EXPECT_FALSE(x);
+    EXPECT_EQ(living_count, 0);
+    EXPECT_EQ(dtor_count, 0);
 }
 
-TEST(Optional, nonEmptyUnassignable) {
+TEST_F(UnassignableTest, nonEmptyUnassignable) {
     int y = 2;
     Unassignable u{y};
+    EXPECT_EQ(living_count, 1);
     optional<Unassignable> x = u;
     EXPECT_TRUE(x);
     EXPECT_EQ(x->x, y);
+    EXPECT_EQ(living_count, 2);
+    EXPECT_EQ(dtor_count, 0);
 }
+
+TEST_F(UnassignableTest, assignUnassignable) {
+    int y = 2;
+    Unassignable u{y};
+    optional<Unassignable> x{};
+    EXPECT_FALSE(x);
+    EXPECT_EQ(living_count, 1);
+    EXPECT_EQ(dtor_count, 0);
+    x = u;
+    EXPECT_TRUE(x);
+    EXPECT_EQ(x->x, y);
+    EXPECT_EQ(living_count, 2);
+    EXPECT_EQ(dtor_count, 0);
+}
+
+TEST_F(UnassignableTest, replaceUnassignable) {
+    int y = 2;
+    int z = 3;
+    Unassignable u{y};
+    Unassignable v{z};
+    optional<Unassignable> x{};
+    EXPECT_FALSE(x);
+    EXPECT_EQ(living_count, 2);
+    EXPECT_EQ(dtor_count, 0);
+    x = u;
+    EXPECT_TRUE(x);
+    EXPECT_EQ(x->x, y);
+    EXPECT_EQ(living_count, 3);
+    EXPECT_EQ(dtor_count, 0);
+    x = v;
+    EXPECT_TRUE(x);
+    EXPECT_EQ(x->x, z);
+    EXPECT_EQ(living_count, 3);
+    EXPECT_EQ(dtor_count, 1);
+}
+
+// TEST(Optional, emptyReference) {
+//     optional<int&> x;
+//     EXPECT_FALSE(x);
+// }
+
+// TEST(Optional, nonEmptyReference) {
+//     int y = 2;
+//     optional<int&> x = y;
+//     EXPECT_TRUE(x);
+//     EXPECT_EQ(*x, y);
+// }
